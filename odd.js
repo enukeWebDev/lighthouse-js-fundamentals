dab32e8cea6c4c93ab27dfc3c65cd570ffc0646b
Roderick Nuque <enuke4@gmail.com>
2021-10-01 15:39:57 +0000

    Updated - odd.js file

--- odd.js
+++ odd.js
@@ -1,3 +1,4 @@
+/*
 function isOdd(num) {
   if (num % 2 !== 0) {
     return true;
@@ -8,3 +9,6 @@ function isOdd(num) {
 //isOdd(3);
 console.log('3 is odd: ' + isOdd(3));
 console.log('8 is odd: ' + isOdd(8));
+*/
+
+
